c02000e416b06eb7f8cd2a0116d1cb00209341ea
Yulia Batyushkova <y-bat@bk.ru>
2020-07-18 02:11:57 +0300

    custom pipe for lists filtration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  e: number = Math.E
-  str: string = 'hello world'
-  date: Date = new Date()
-  float: number = 0.42
-  obj = {
-    a: 1,
-    b: {
-      c: 2,
-      d: {
-        e: 3,
-        f: 4
-      }
-    }
-  }
+
+  search = ''
+  searchField = 'title'
+  
+ posts: Post[] = [
+   {title: 'Beer', text: 'Best drink ever!'},
+   {title: 'Wine', text: 'I love it :)'},
+   {title: 'Water', text: 'So yammy'}
+ ]
+
+ addPost() {
+   this.posts.unshift({
+     title: 'Angular 8',
+     text: 'Angular 8 course'
+   })
+ }
 }
